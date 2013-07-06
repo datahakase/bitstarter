9c62e4d543a886a71a6433a377775306f8fb4cd5
unknown <datahakase@outlook.com>
2013-07-06 10:58:25 +0900

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express();
 
+var pageContent = "...";
+
+fs.readFile('./index.html', function (err, data) {
+    if (err) throw err;
+    pageContent = data.toString();
+});
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send(pageContent);
 });
 
 var port = process.env.PORT || 5000;
